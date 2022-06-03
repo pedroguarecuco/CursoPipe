c5c6da02b9aeebb3fdef4db7f9034b7559584335
pguarecucoGforceint <pguarecuco@gforceint.com>
2022-06-03 18:48:56 -0500

    Inicial

---
+++ Controllers/AgendaController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoAzure.Controllers
+{
+    public class AgendaController : Controller
+    {
+        // GET: AgendaController
+        public ActionResult Index()
+        {
+            List<CursoAzure.Models.AgendaModels> lista= new List<CursoAzure.Models.AgendaModels>();
+            CursoAzure.Models.AgendaModels model = new Models.AgendaModels();
+            model.Id= 1;
+            model.Nombre = "Pedro Guarecuco";
+            lista.Add(model);
+            model.Id = 2;
+            model.Nombre = "Jose Guarecuco";
+            lista.Add(model);
+            return View(lista);
+        }
+
+        // GET: AgendaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AgendaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AgendaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AgendaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AgendaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AgendaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AgendaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/AgendaModels.cs
@@ -0,0 +1,8 @@
+﻿namespace CursoAzure.Models
+{
+    public class AgendaModels
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
